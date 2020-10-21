90345204a2923ef98b51dc052ebaa9f4a4347736
fj <castelloiv@gmail.com>
2020-10-21 15:33:53 -0400

    Added my work flow image

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,20 @@
 import React from "react";
-
+import Flow from "../../images/flow3.svg";
 import Styles from "./home.module.scss";
 const Home = () => {
   return (
-    <section className={Styles.hero}>
-      <div className={Styles.heroCon}></div>
-    </section>
+    <>
+      <section className={Styles.hero}>
+        <div className={Styles.heroCon}></div>
+      </section>
+      <section className={Styles.flow}>
+        <div className={Styles.con}>
+          <div className={Styles.imgcon}>
+            <img src={Flow} alt="Diagram of servers" />
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
